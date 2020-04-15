575023641366d81fdadbec0c537de2a7327a1ff4
Juliet George <georgejuliet11@gmail.com>
2020-04-14 23:54:49 -0700

    update team member js files

---
+++ Develop/lib/Intern.js
@@ -0,0 +1,26 @@
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+var Employee = require("./Employee");
+
+
+class Intern extends Employee{
+    constructor(name, id, email, github, school){
+        super(name,id,email);
+        this.github = github;
+        this.school = school;
+    }
+    getGithub(){
+        return this.github;
+
+    }
+
+    getSchool(){
+        return this.school;
+
+
+    }
+
+    getRole(){
+        return "Intern";
+    }
+}
+modeule.exports = Intern;
