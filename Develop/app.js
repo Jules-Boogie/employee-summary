5f5b94477437a09ee0e4709aa75a167b854dc5e8
Juliet George <georgejuliet11@gmail.com>
2020-04-15 16:32:09 -0700

    all the code to generate html

--- Develop/app.js
+++ Develop/app.js
@@ -9,65 +9,168 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const employee = [];
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-function teamMember(){
-inquirer.prompt([
-    {   type:"input",
-        message:"What is the team member's name?",
-        name:"name"
-    },
-    {
-        type:"list",
-        message:"what is the role of the team member?",
-        name:"role"
+function teamMember() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the team member's name?",
+            name: "name"
+        },
+        {
+            type: "list",
+            message: "what is the role of the team member?",
+            name: "role"
 
-    },
-    {
-        type:"input",
-        message:"what is their email address?",
-        name:"email"
+        },
+        {
+            type: "input",
+            message: "what is their email address?",
+            name: "email"
 
-    },
-    {
-        type:"input",
-        message:"Enter Team member's ID",
-        name:"id"
-    }
-])
-.then(function({name,role,email,id}){
+        },
+        {
+            type: "input",
+            message: "Enter Team member's ID",
+            name: "id"
+        }
+    ])
+        .then(function ({ name, role, email, id }) {
+            let teamRoleInfo;
+            if (role === "Engineer") {
+                teamRoleInfo = "Github username";
+            } else if (role === "Intern") {
+                teamRoleInfo = "school name";
+            } else {
+                teamRoleInfo = "Office Phone Number"
+            }
+            inquirer.prompt([
+                {   type:"input",
+                    message:`Team members ${teamRoleInfo}`,
+                    name:"teamRoleInfo"
 
+                },
+                {
+                    type:"list",
+                    message: "Do you want to add another team member?",
+                    choices:["yes","no" ],
+                    name:"addMembers"
+                }
+
+
+
+            ])
+            .then(function({teamRoleInfo,addMembers}){
+                let newEmployee;
+                if(role === "Intern"){
+
+                    newEmployee = new Intern(name, id, email, teamRoleInfo);
+                } else if(role === "Engineer"){
+                    newEmployee = new Engineer(name,id,email,teamRoleInfo)
+                } else{
+                    newEmployee = new Manager(name,id,email, teamRoleInfo)
+                }
+                employee.push(newEmployee);
+                addtoHTML(newEmployee)
+                .then(function(){
+                    if(addMembers === "yes"){
+                        teamMember();
+                    } else{
+                        endHtml();
+                    }
+
+                })
+
+
+            })
+
+
+
+
+        }
+
+
+
+
+        )
+
+}
+
+function init(){
+    createHTML()
+    teamMember()
+}
+   
+
+
+
+
+
+function createHTML() {
+    const html = `<!doctype html>
+    <html lang="en">
+      <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
+        <title>Team Profile</title>
+      </head>
+      <body>
+        <h1 class="jumbotron Display-3 text-center" >Welcome to Employee DashBoard</h1>
+        <div >
+    
+    
+    `
+    fs.writeFile("employee.html", html, function(err){
+        if(err){
+            throw err;
+        }
+        console.log("Your HTML FILE HAS BEEN GENERATED")
+        
+    })
 
 
 
 }
 
 
+function endHtml(){
+    const end = `
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
+    
+    `
+    fs.appendFile("employee.html", end, function(err){
+        if(err){
+            throw err;
+        }
+        console.log("Your HTML FILE HAS BEEN Closed")
 
+    })
 
-)
 
 }
 
-
-
-function createHTML(){
-
-
-
-}
-
-function addtoHTML(team){
+function addtoHTML(team) {
     const name = team.getName();
     const email = team.getEmail();
     const id = team.getId();
     const role = team.getRole();
     let teamInfo = "";
 
-    if(role === "Engineer"){
+    if (role === "Engineer") {
         const gitHub = member.getGithub();
         teamInfo = `<div class="col">
         <div class="card mx-auto mb-3">
@@ -79,7 +182,7 @@ function addtoHTML(team){
       </ul>
       </div> `
 
-    } else if(role === "Manager"){
+    } else if (role === "Manager") {
         const officeNumber = team.getofficeNumber();
         teamInfo = `
         <div class="col">
@@ -110,7 +213,7 @@ function addtoHTML(team){
 
     }
 
-    fs.appendFile("team.html", teamInfo, function(err){
+    fs.appendFile("team.html", teamInfo, function (err) {
         if (err) {
             throw err;
         }
@@ -122,7 +225,7 @@ function addtoHTML(team){
 
 
 
-    
+
 
 
 
@@ -146,7 +249,7 @@ function addtoHTML(team){
 
 
 
-
+init();
 
 
 
